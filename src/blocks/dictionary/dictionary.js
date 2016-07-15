9e63c8fedf98be784a78e55b89bda33dae47c460
Eldar Khaitov <Eldar_Khaitov@epam.com>
2016-07-15 22:01:34 +0300

    Basic word adding implemented

--- src/blocks/dictionary/dictionary.js
+++ src/blocks/dictionary/dictionary.js
@@ -15,29 +15,61 @@
 
         function dictionaryController() {
             var vm = this;
+            var newWord = {};
+            var dictionary = JSON.parse(localStorage.dictionary || "[]");
 
             angular.extend(vm, {
-                addNewWord
+                addWord,
+                removeWord,
+                saveWords,
+                clearWords,
+                newWord
             });
 
-            try {
-                var rawDictionary = JSON.parse(localStorage.dictionary || "[]");
-                vm.dictionary = rawDictionary.map((element) => {
-                    var newElement = element;
-                    newElement.translations = element.translations.join(', ');
-                    return newElement;
-                });
-                // vm.dictionary = viewDictionary;
-            } catch(e) {
-                console.log(e);
+            fillWords();
+
+            function addWord() {
+                var word = angular.copy(vm.newWord);
+                word.translations = newWord.translations.split(/,\s*|^\s+|\s+$/);
+                dictionary.splice(0, 0, word);
+                fillWords();
             }
 
-            function addNewWord() {
+            function removeWord(index) {
+                dictionary.splice(index, 1);
+                fillWords();
+            }
 
+            function fillWords() {
+                try {
+                    vm.dictionary = dictionary.map((element) => {
+                        var newElement = angular.copy(element);
+                        newElement.translations = element.translations.join(', ');
+                        return newElement;
+                    });
+                } catch(e) {
+                    //TODO: make pretty inform
+                    console.log(e);
+                }
+            }
+
+            function saveWords() {
+                localStorage.dictionary = JSON.stringify(dictionary);
+            }
+
+            function clearWords() {
+                dictionary = JSON.parse(localStorage.dictionary || "[]");
+                vm.newWord = {
+                    word: '',
+                    transcription: '',
+                    translations: []
+                };
+                fillWords();
             }
         }
 
         function link() {
+            //TODO: make new page title
             uiService.pageTitle = 'Add New Words'
         }
     }
